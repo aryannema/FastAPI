cc4862096268a43eac8048624f4f168bb55494bb
aryannema <aaryan.techy@gmail.com>
2025-06-22 12:54:57 +0530

    project 1

---
+++ Project1/books.py
@@ -0,0 +1,101 @@
+# fastapi looks at all the functions in a chronological order.
+# %20 is considered as a space in the url.
+# get request dont have a Body.
+
+
+from fastapi import Body, FastAPI
+
+app = FastAPI()
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
+
+
+#Path parameter
+@app.get('/books/{book_title}')
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+        
+
+# query parameters:
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    
+    return books_to_return
+
+
+
+# Assignment:
+# to get all the books from a specific author using path or query parameter
+
+@app.get("/books/byauthor/")
+async def read_books_by_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+    
+    return books_to_return
+
+
+
+
+# combining query and path parameters:
+@app.get('/books/{book_author}/')
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    
+
+    return books_to_return
+
+
+
+# post http method: used to create data:
+@app.post("/books/create_book")
+async def create_book(new_book = Body()):
+    BOOKS.append(new_book)
+
+
+
+# put request:
+@app.put("/books/update_books")
+async def update_books(updated_book = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+
+# delete request:
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+
+
+
+
